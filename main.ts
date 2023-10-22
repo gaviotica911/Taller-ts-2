79e1718ba934bdc78f76a04d8b2e6e583e13213d
Gabriela Garcia <g.garcias2@uniandes.edu.co>
2023-10-22 00:35:39 -0500

    fin

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import {series} from './data.js'
 
 let seriesTable: HTMLElement = document.getElementById("series")!;
 let promedioElemento : HTMLElement = document.getElementById("promedio")!;
+const card = document.getElementById("cardS")!;
 
 mostrarSeries(series);
 mostrarPromedio(series);
@@ -13,7 +14,7 @@ function mostrarSeries(series:Serie[]) :void{
     {
         let trElement:HTMLElement=document.createElement("tr");
         trElement.innerHTML=`<td>${serie.id}</td>
-     <td>${serie.titulo}</td>
+        <td><a class="titulo-pelicula" data-more="${serie.url}" data-imagen="${serie.img}" data-descripcion="${serie.descripcion}">${serie.titulo}</a></td>
         <td>${serie.plataforma}</td>
        <td>${serie.temporadas}</td>`;
         seriesTbody.appendChild(trElement);
@@ -21,7 +22,41 @@ function mostrarSeries(series:Serie[]) :void{
 
     seriesTable.appendChild(seriesTbody);
 
+    const tituloPeliculas = document.querySelectorAll(".titulo-pelicula");
+  
+    tituloPeliculas.forEach((titulo) => {
+        titulo.addEventListener("click", () => {
+            const imagen = titulo.getAttribute("data-imagen")!;
+            console.log(imagen);
+            const descripcion = titulo.getAttribute("data-descripcion")!;
+            const url=titulo.getAttribute("data-more")!;
+
+            const cardImg = card.querySelector(".card-img-top") as HTMLImageElement ;
+            const cardTitle = card.querySelector(".card-title") as HTMLHeadingElement;
+            const cardText = card.querySelector(".card-text") as HTMLParagraphElement;
+            const cardMore = card.querySelector(".more") as HTMLAnchorElement;
+
+            cardImg.src = imagen;
+            console.log(cardImg.src);
+            cardTitle.textContent = titulo.textContent!;
+            cardText.textContent = descripcion;
+            cardMore.href = url;
+
+            card.style.display = "block";
+        });
+
+    });
+
+
+
+
+
 }
+
+
+
+
+
 
 function promedio(series:Serie[]):number{
         let temporadas:number = 0;
@@ -38,3 +73,24 @@ function mostrarPromedio(series:Serie[]):void{
     trElement.innerHTML = `<td><b>Promedio por temporada: </b></td><td>${prom}</td>`;
     promedioElemento.appendChild(trElement);
 }
+
+seriesTable.addEventListener("click", (event) => {
+    const target = event.target as HTMLTableCellElement;
+    if (target.classList.contains("titulo-pelicula")) {
+        const imagen = target.getAttribute("data-imagen");
+        const descripcion = target.getAttribute("data-descripcion");
+
+        const cardImg = card?.querySelector(".card-img-top") as HTMLImageElement;
+        const cardTitle = card?.querySelector(".card-title") as HTMLHeadingElement;
+        const cardText = card?.querySelector(".card-text") as HTMLParagraphElement;
+
+        if (cardImg && cardTitle && cardText && card) {
+            cardImg.src = imagen || "";
+            cardTitle.textContent = target.textContent || "";
+            cardText.textContent = descripcion || "";
+
+            card.style.display = "block";
+        }
+    }
+});
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { series } from './data.js';
 var seriesTable = document.getElementById("series");
 var promedioElemento = document.getElementById("promedio");
+var card = document.getElementById("cardS");
 mostrarSeries(series);
 mostrarPromedio(series);
 function mostrarSeries(series) {
@@ -8,10 +9,29 @@ function mostrarSeries(series) {
     for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
         var serie = series_1[_i];
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n     <td>").concat(serie.titulo, "</td>\n        <td>").concat(serie.plataforma, "</td>\n       <td>").concat(serie.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n        <td><a class=\"titulo-pelicula\" data-more=\"").concat(serie.url, "\" data-imagen=\"").concat(serie.img, "\" data-descripcion=\"").concat(serie.descripcion, "\">").concat(serie.titulo, "</a></td>\n        <td>").concat(serie.plataforma, "</td>\n       <td>").concat(serie.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
     }
     seriesTable.appendChild(seriesTbody);
+    var tituloPeliculas = document.querySelectorAll(".titulo-pelicula");
+    tituloPeliculas.forEach(function (titulo) {
+        titulo.addEventListener("click", function () {
+            var imagen = titulo.getAttribute("data-imagen");
+            console.log(imagen);
+            var descripcion = titulo.getAttribute("data-descripcion");
+            var url = titulo.getAttribute("data-more");
+            var cardImg = card.querySelector(".card-img-top");
+            var cardTitle = card.querySelector(".card-title");
+            var cardText = card.querySelector(".card-text");
+            var cardMore = card.querySelector(".more");
+            cardImg.src = imagen;
+            console.log(cardImg.src);
+            cardTitle.textContent = titulo.textContent;
+            cardText.textContent = descripcion;
+            cardMore.href = url;
+            card.style.display = "block";
+        });
+    });
 }
 function promedio(series) {
     var temporadas = 0;
@@ -27,3 +47,19 @@ function mostrarPromedio(series) {
     trElement.innerHTML = "<td><b>Promedio por temporada: </b></td><td>".concat(prom, "</td>");
     promedioElemento.appendChild(trElement);
 }
+seriesTable.addEventListener("click", function (event) {
+    var target = event.target;
+    if (target.classList.contains("titulo-pelicula")) {
+        var imagen = target.getAttribute("data-imagen");
+        var descripcion = target.getAttribute("data-descripcion");
+        var cardImg = card === null || card === void 0 ? void 0 : card.querySelector(".card-img-top");
+        var cardTitle = card === null || card === void 0 ? void 0 : card.querySelector(".card-title");
+        var cardText = card === null || card === void 0 ? void 0 : card.querySelector(".card-text");
+        if (cardImg && cardTitle && cardText && card) {
+            cardImg.src = imagen || "";
+            cardTitle.textContent = target.textContent || "";
+            cardText.textContent = descripcion || "";
+            card.style.display = "block";
+        }
+    }
+});
